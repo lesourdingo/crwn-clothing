a4266f9ce977b2120c6646f57af35efa7fdc2738
Kevin Rumeur <lesourdingo@gmail.com>
2020-02-06 12:14:20 +0100

    created cart-item component and connected our card-dropdown with cartItems reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,6 +1,6 @@
 import CartActionTypes from './cart.types';
 
-const toggleCartHidden = () => ({
+export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
@@ -8,5 +8,3 @@ export const addItem = item => ({
   type: CartActionTypes.ADD_ITEM,
   payload: item
 });
-
-export default toggleCartHidden;
